dc193f79e62f7819519ff52bb4757b41219aa1ed
Mikita <nikita1597538426@gmail.com>
2020-10-04 19:56:32 +0300

    workers empl

--- firts_bot.py
+++ firts_bot.py
@@ -1,13 +1,23 @@
 import sc2
 from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
-from sc2.constants import NEXUS, PROBE
+from sc2.constants import NEXUS, PROBE, PYLON
 
 
 class myBot(sc2.BotAI):
 	async def on_step(self, iteration):
 		await self.distribute_workers()
 		await self.buid_workers()
+		await self.buid_pylon()
+
+	async def buid_pylon(self):
+		for 
+
+	async def buid_workers(self):
+		for nexus in self.units(NEXUS).ready.noqueue:
+			if self.can_afford(PROBE):
+				await self.do(nexus.train(PROBE))
+
 
 
 run_game(maps.get('AbyssalReefLE'), [
