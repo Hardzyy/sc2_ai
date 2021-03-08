ba09a03a461d0854491fd08f4cf634fee6e53038
Mikita <nikita1597538426@gmail.com>
2021-03-08 16:44:20 +0300

    refactored with async methods, functionality same

--- zerg_bot.py
+++ zerg_bot.py
@@ -4,7 +4,7 @@ import sc2
 from sc2.position import Point2, Point3
 from sc2.bot_ai import BotAI
 from sc2.player import Bot, Computer
-from sc2.ids.unit_typeid import UnitTypeId
+# from sc2.ids.unit_typeid import UnitTypeId
 from sc2.unit import Unit
 from sc2.constants import *
 
@@ -31,30 +31,41 @@ class ZergBot(BotAI):
                 print(Exception)
 
         hatch: Unit = self.townhalls[0]
+        target: Point2 = self.enemy_structures.not_flying.random_or(self.enemy_start_locations[0]).position
 
-        if(
-            self.gas_buildings.amount + self.already_pending(UnitTypeId.EXTRACTOR) == 0
-            and self.can_afford(UnitTypeId.EXTRACTOR)
-            and self.workers
+        await self.create_gas()
+        await self.harvest_gas()
+        await self.create_workers()
+        await self.build_spawning(hatch)
+        await self.push(target)
+        await self.spawn_overlord()
+        await self.get_movespeed()
+        await self.create_queen()
+        await self.create_zergling()
+        await self.create_lavra(hatch)
+
+    async def create_lavra(self, hatch):
+        for queen in self.units(UnitTypeId.QUEEN):
+            if queen.energy >= 25 and not hatch.has_buff(BuffId.QUEENSPAWNLARVATIMER):
+                queen(AbilityId.EFFECT_INJECTLARVA, hatch)
+
+    async def create_zergling(self):
+        if (
+            self.can_afford(UnitTypeId.ZERGLING)
+            and self.structures(UnitTypeId.SPAWNINGPOOL).ready
+            and self.already_pending_upgrade(UpgradeId.ZERGLINGMOVEMENTSPEED)
         ):
-            worker: Unit = self.workers.random
-            gas: Unit = self.vespene_geyser.closest_to(worker)
-            worker.build_gas(gas)
+            self.train(UnitTypeId.ZERGLING)
 
-        if(
-            self.gas_buildings.ready
-            and self.workers
+    async def create_queen(self):
+        if (
+            self.can_afford(UnitTypeId.QUEEN)
+            and self.units(UnitTypeId.QUEEN).amount + self.already_pending(UnitTypeId.QUEEN) < self.townhalls.amount
+            and self.structures(UnitTypeId.SPAWNINGPOOL).ready
         ):
-            gas: Unit = self.gas_buildings.first
-            if gas.surplus_harvesters < 0:
-                self.workers.random.gather(gas)
+            self.train(UnitTypeId.QUEEN)
 
-        if(
-            self.can_afford(UnitTypeId.DRONE)
-            and self.supply_workers < 19
-        ):
-            self.train(UnitTypeId.DRONE)
-
+    async def build_spawning(self, hatch):
         if(
             self.can_afford(UnitTypeId.SPAWNINGPOOL)
             and not self.already_pending(UnitTypeId.SPAWNINGPOOL)
@@ -66,14 +77,30 @@ class ZergBot(BotAI):
                     worker: Unit = self.workers.closest_to(pos)
                     worker.build(UnitTypeId.SPAWNINGPOOL, pos)
 
-        if(
+    async def spawn_overlord(self):
+        if (
             self.supply_left < 2
             and self.can_afford(UnitTypeId.OVERLORD)
             and self.already_pending(UnitTypeId.OVERLORD) < 1
         ):
             self.train(UnitTypeId.OVERLORD)
 
-        if(
+    async def create_workers(self):
+        if (
+            self.can_afford(UnitTypeId.DRONE)
+            and not self.already_pending(UnitTypeId.DRONE)
+            and self.supply_workers < 18
+        ):
+            self.train(UnitTypeId.DRONE)
+
+    # Give all zerglings an attack command
+    async def push(self, target):
+        if self.units(UnitTypeId.ZERGLING).amount > 14:
+            for zergling in self.units(UnitTypeId.ZERGLING):
+                zergling.attack(target)
+
+    async def get_movespeed(self):
+        if (
             self.already_pending_upgrade(UpgradeId.ZERGLINGMOVEMENTSPEED) == 0
             and self.can_afford(UpgradeId.ZERGLINGMOVEMENTSPEED)
         ):
@@ -81,31 +108,25 @@ class ZergBot(BotAI):
             if spawn_pool:
                 self.research(UpgradeId.ZERGLINGMOVEMENTSPEED)
 
-        if(
-            self.can_afford(UnitTypeId.QUEEN)
-            and self.units(UnitTypeId.QUEEN).amount + self.already_pending(UnitTypeId.QUEEN) < self.townhalls.amount
-            and self.structures(UnitTypeId.SPAWNINGPOOL).ready
-        ):
-            self.train(UnitTypeId.QUEEN)
-
-        for queen in self.units(UnitTypeId.QUEEN):
-            if queen.energy >= 25 and not hatch.has_buff(BuffId.QUEENSPAWNLARVATIMER):
-                queen(AbilityId.EFFECT_INJECTLARVA, hatch)
-
+    async def harvest_gas(self):
         if (
-            self.can_afford(UnitTypeId.ZERGLING)
-            and self.structures(UnitTypeId.SPAWNINGPOOL).ready
-            and self.already_pending_upgrade(UpgradeId.ZERGLINGMOVEMENTSPEED)
+            self.gas_buildings.ready
+            and self.workers
         ):
-            self.train(UnitTypeId.ZERGLING)
+            gas: Unit = self.gas_buildings.first
+            if gas.surplus_harvesters < 0:
+                self.workers.random.gather(gas)
 
-        target: Point2 = self.enemy_structures.not_flying.random_or(self.enemy_start_locations[0]).position
-
-        # Give all zerglings an attack command
-        if self.units(UnitTypeId.ZERGLING).amount > 14:
-            for zergling in self.units(UnitTypeId.ZERGLING):
-                zergling.attack(target)
-
+    # creating extractor if doesn't exists and can apply
+    async def create_gas(self):
+        if (
+            self.gas_buildings.amount + self.already_pending(UnitTypeId.EXTRACTOR) == 0
+            and self.can_afford(UnitTypeId.EXTRACTOR)
+            and self.workers
+        ):
+            worker: Unit = self.workers.random
+            gas: Unit = self.vespene_geyser.closest_to(worker)
+            worker.build_gas(gas)
 
 
 if __name__ == '__main__':
@@ -113,6 +134,6 @@ if __name__ == '__main__':
     dis_con = subprocess.run(dis_string, capture_output=True, text=True).stdout
     sc2.run_game(
         sc2.maps.get("AcropolisLE"),
-        [Bot(sc2.Race.Zerg, ZergBot()), Computer(sc2.Race.Zerg, sc2.Difficulty.Hard)],
+        [Bot(sc2.Race.Zerg, ZergBot()), Computer(sc2.Race.Zerg, sc2.Difficulty.Medium)],
         realtime=False,
     )
