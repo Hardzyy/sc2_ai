7b730e7d4b524fd21ebd4cf3be695ae20e294c06
Mikita <nikita1597538426@gmail.com>
2021-03-10 12:32:55 +0300

    a few comments and fixes

--- zerg_bot.py
+++ zerg_bot.py
@@ -17,11 +17,13 @@ class ZergBot(BotAI):
         self.wifi_state = True
         self.wifi_ssid = "system_ANAList"
         self.wifi_name = "system_ANAList"
+        self.ITER_PER_MIN = 165
 
     async def on_step(self, iteration: int):
         # windows only!!!
         # сделано чтобы отключаться от wi-fi в момент запуска
         # если не сделать, теряем время из-за ping'ов до сервера
+        self.iteration = iteration
         if self.wifi_state:
             try:
                 con_string = "netsh wlan connect ssid=system_ANAList name=system_ANAList"
@@ -32,21 +34,21 @@ class ZergBot(BotAI):
 
         hatch: Unit = self.townhalls[0]
         target: Point2 = self.enemy_structures.not_flying.random_or(self.enemy_start_locations[0]).position
-
+        # создаем все козявки
         await self.create_workers()
         await self.create_overlord()
         await self.create_gas()
         await self.create_queen()
         await self.create_lavra(hatch)
         await self.create_zergling()
-
+        #
         await self.harvest_gas()
         await self.get_movespeed()
 
         await self.build_spawning(hatch)
         await self.push(target)
 
-    async def create_lavra(self, hatch):
+    async def create_lavra(self, hatch: Unit):
         for queen in self.units(UnitTypeId.QUEEN):
             if queen.energy >= 25 and not hatch.has_buff(BuffId.QUEENSPAWNLARVATIMER):
                 queen(AbilityId.EFFECT_INJECTLARVA, hatch)
@@ -67,7 +69,7 @@ class ZergBot(BotAI):
         ):
             self.train(UnitTypeId.QUEEN)
 
-    async def build_spawning(self, hatch):
+    async def build_spawning(self, hatch: Unit):
         if(
             self.can_afford(UnitTypeId.SPAWNINGPOOL)
             and not self.already_pending(UnitTypeId.SPAWNINGPOOL)
@@ -137,5 +139,5 @@ if __name__ == '__main__':
     sc2.run_game(
         sc2.maps.get("AcropolisLE"),
         [Bot(sc2.Race.Zerg, ZergBot()), Computer(sc2.Race.Zerg, sc2.Difficulty.Medium)],
-        realtime=False,
+        realtime=True,
     )
